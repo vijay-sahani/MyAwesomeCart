4bc2e536073ff1ae6c144041e72e3c0484abb9f7
vijay <sahanivijay800@gmail.com>
2021-03-18 00:06:22 +0530

    Add files via upload

--- mac_/settings.py
+++ mac_/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEADIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+STATIC_URL=os.path.join(BASE_DIR,'static')
+
+django_heroku.settings(locals())
